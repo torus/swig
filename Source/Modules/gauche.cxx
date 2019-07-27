772e652d24ecebb7be59d49d752c019c9b603bb5
Toru Hisai <toru@torus.jp>
2019-07-27 15:03:08 +0200

    adding non-language-specific code

--- Source/Modules/gauche.cxx
+++ Source/Modules/gauche.cxx
@@ -1,20 +1,92 @@
 #include "swigmod.h"
 
 class GAUCHE : public Language {
+protected:
+  /* General DOH objects used for holding the strings */
+  File *f_begin;
+  File *f_runtime;
+  File *f_header;
+  File *f_wrappers;
+  File *f_init;
+
 public:
 
-    virtual void main(int argc, char *argv[]) {
-        printf("I'm the Gauche module.\n");
-    }
+  virtual void main(int argc, char *argv[]) {
+    printf("I'm the Gauche module.\n");
 
-    virtual int top(Node *n) {
-        printf("Generating code.\n");
-        return SWIG_OK;
-    }
+    /* Set language-specific subdirectory in SWIG library */
+    SWIG_library_directory("gauche");
+
+    /* Set language-specific preprocessing symbol */
+    Preprocessor_define("SWIGGAUCHE 1", 0);
+
+    /* Set language-specific configuration file */
+    // SWIG_config_file("gauche.swg");
+
+    /* Set typemap language (historical) */
+    SWIG_typemap_lang("gauche");
+
+  }
+
+  virtual int top(Node *n);
 
 };
 
 extern "C" Language *
 swig_gauche(void) {
-    return new GAUCHE();
+  return new GAUCHE();
 }
+
+int GAUCHE::top(Node *n) {
+  printf("Generating code.\n");
+
+  /* Get the module name */
+  String *module = Getattr(n, "name");
+
+  /* Get the output file name */
+  String *outfile = Getattr(n, "outfile");
+
+  /* Initialize I/O (see next section) */
+  f_begin = NewFile(outfile, "w", SWIG_output_files());
+  if (!f_begin) {
+    FileErrorDisplay(outfile);
+    SWIG_exit(EXIT_FAILURE);
+  }
+  f_runtime = NewString("");
+  f_init = NewString("");
+  f_header = NewString("");
+  f_wrappers = NewString("");
+
+  /* Register file targets with the SWIG file handler */
+  Swig_register_filebyname("begin", f_begin);
+  Swig_register_filebyname("header", f_header);
+  Swig_register_filebyname("wrapper", f_wrappers);
+  Swig_register_filebyname("runtime", f_runtime);
+  Swig_register_filebyname("init", f_init);
+
+  /* Output module initialization code */
+  Swig_banner(f_begin);
+
+  /* Emit code for children */
+  Language::top(n);
+
+  /* Write all to the file */
+  Dump(f_runtime, f_begin);
+  Dump(f_header, f_begin);
+  Dump(f_wrappers, f_begin);
+  Wrapper_pretty_print(f_init, f_begin);
+
+  /* Cleanup files */
+  Delete(f_runtime);
+  Delete(f_header);
+  Delete(f_wrappers);
+  Delete(f_init);
+  Delete(f_begin);
+
+  return SWIG_OK;
+}
+
+// Local Variables:
+// mode: c++
+// c-basic-offset: 2
+// End:
